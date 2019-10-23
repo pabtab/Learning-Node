c5ddd50907545398cec22f4a7610ebf0ff537e55
Pablo Tabares Hernandez <pablo.tabares@globant.com>
2019-10-23 15:10:48 -0500

    bootstrap handlebars express

--- 05-web-server/server.js
+++ 05-web-server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs')
 
-app.get('/', function (req, res) {
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+/* app.get('/', function (req, res) {
   const response = {
     name: 'pabtab',
     age: 26,
@@ -10,10 +15,21 @@ app.get('/', function (req, res) {
 
   res.send(response);
 })
+ */
+app.get('/', function (req, res) {
 
-app.get('/data', function (req, res) {
-
-  res.send('Hello Data');
+  res.render('home', {
+    name: 'Pabtab',
+    year: new Date().getFullYear()
+  });
 })
+
+app.get('/about', function (req, res) {
+
+  res.render('about', {
+    year: new Date().getFullYear()
+  });
+})
+
 
 app.listen(3000, () => console.log('Listening 300'));
