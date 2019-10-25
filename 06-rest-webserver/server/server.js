4cd7af06f7f9203e18d4aa6ad41aff813b4e79ef
Pablo Tabares Hernandez <pablother3@gmail.com>
2019-10-24 19:09:12 -0500

    endpoints did it

--- 06-rest-webserver/server/server.js
+++ 06-rest-webserver/server/server.js
@@ -1,8 +1,38 @@
+require('./config/config')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
  
-app.get('/', function (req, res) {
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.get('/user', function (req, res) {
   res.json('Hello World')
 })
+
+app.post('/user', function (req, res) {
+  const body = req.body;
+
+  if (!body.name) {
+    res.status(400).json({
+      ok: false,
+      message: 'Name required'
+    })
+  }
+  res.json(body)
+})
+
+app.put('/user/:id', function (req, res) {
+  const id = req.params.id;
+
+  res.json({ id })
+})
+
+app.delete('/user', function (req, res) {
+  res.json('delete')
+})
  
-app.listen(3000, () => console.log('listening 3000'))
+app.listen(process.env.PORT, () => console.log(`listening ${process.env.PORT}`))
